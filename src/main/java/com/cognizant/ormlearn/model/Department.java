a6a9277dc9cdd2335016d391cab35b07dcf82b1b
Vijai <vijayselvanvj@gmail.com>
2021-06-20 15:08:10 +0530

    04-02_Create payroll tables and bean mapping

---
+++ src/main/java/com/cognizant/ormlearn/model/Department.java
@@ -0,0 +1,32 @@
+package com.cognizant.ormlearn.model;
+
+import java.util.Set;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "department")
+public class Department {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "dp_id")
+	private int id;
+
+	@Column(name = "dp_name")
+	private String name;
+
+	@OneToMany(mappedBy = "department", fetch = FetchType.EAGER)
+	private Set<Employee> employeeList;
+
+}
---
+++ src/main/java/com/cognizant/ormlearn/model/Skill.java
@@ -0,0 +1,31 @@
+package com.cognizant.ormlearn.model;
+
+import java.util.Set;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToMany;
+import javax.persistence.Table;
+
+import lombok.Data;
+
+@Entity
+@Table(name = "skill")
+@Data
+public class Skill {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "sk_id")
+	private int id;
+
+	@Column(name = "sk_name")
+	private String name;
+
+	@ManyToMany(mappedBy = "skillList")
+	private Set<Employee> employeeList;
+	
+}
